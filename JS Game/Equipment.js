ab40f7d0c495a2fb92822756aef90ac3328b45b9
Chase Lee <chase.seeberger@gmail.com>
2022-10-26 15:58:13 -0600

    clean up equipment.js

--- JS Game/Equipment.js
+++ JS Game/Equipment.js
@@ -1,4 +1,13 @@
-//Equipment constructor
+
+/* 
+creates new equipment object called in itemArray[]
+type
+0 = none
+1 = helm
+2 = chest
+3 = boots
+*/
+
 class Equipment {
     constructor(name, type, armor) {
         this.name = name;
